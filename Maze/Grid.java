f4cd61ccf6a12b82f54662c2327e5c15685dee07
Ben <ben.tom.vincent@gmail.com>
2019-05-04 15:51:23 -0700

    Maze Draws Grid now

--- Maze/Grid.java
+++ Maze/Grid.java
@@ -2,4 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 public class Grid extends JPanel {
 
+  public void paintComponent(Graphics g) {
+    int CellSize = 10;
+    super.paintComponent(g);
+    setBackground(Color.WHITE);
+    g.setColor(Color.BLACK);
+    //g.drawLine(CellSize, 0, CellSize, 1000);
+    // Draw columns
+    for (int i = 0; i < 1000; i++) {
+      g.drawLine(i * CellSize, 0, i * CellSize, 1000);
+      for (int j = 0; j < 1000; j++) {
+        g.drawLine(0, j * CellSize, 1000, j * CellSize);
+      }
+    }
+
+  }
 }
--- Maze/Maze.java
+++ Maze/Maze.java
@@ -3,18 +3,27 @@ import java.awt.*;
 
 public class Maze {
 
+public static final int WIDTH = 1000;
+public static final int HEIGHT = 1000;
+
   public static void main(String[] args) {
-    drawWindow(500, 500);
+    drawWindow(WIDTH, HEIGHT);
   }
 
   public static void drawWindow(int w, int h) {
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         JFrame frame = new JFrame("Maze Generator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null); // prevents auto sizing
-        frame.setVisible(true);
+
         frame.setSize(w, h);
+        Grid test = new Grid();
+        frame.add(test);
+
+
+        //frame.setLayout(null); // prevents auto sizing
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
       }
     });
   }
