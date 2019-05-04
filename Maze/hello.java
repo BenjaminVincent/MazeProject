91b415005a937a335fd99f3872088566dce07bef
Ben <ben.tom.vincent@gmail.com>
2019-05-04 13:37:33 -0700

    Added window

--- Maze/hello.java
+++ Maze/hello.java
@@ -1,6 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class hello {
+
   public static void main(String[] args) {
-    System.out.println("hello");
-    System.out.println("another line");
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        JFrame frame = new JFrame("Maze Generator");
+        frame.setSize(500, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+      }
+    });
   }
 }
