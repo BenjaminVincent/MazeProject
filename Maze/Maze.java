cdb0032706dcd102b8e1a8ada395e39f77d77f4a
Ben <ben.tom.vincent@gmail.com>
2019-05-04 15:05:52 -0700

    cleaned up and added Grid.java

--- Maze/Maze.java
+++ Maze/Maze.java
@@ -2,21 +2,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Maze {
-  public static int WIDTH = 500;
-  public static int HEIGHT = 500;
 
   public static void main(String[] args) {
-    drawWindow();
+    drawWindow(500, 500);
   }
 
-  public static void drawWindow() {
+  public static void drawWindow(int w, int h) {
     SwingUtilities.invokeLater(new Runnable() {
       public void run() {
         JFrame frame = new JFrame("Maze Generator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null); // prevents auto sizing
         frame.setVisible(true);
-        frame.setSize(WIDTH, HEIGHT);
+        frame.setSize(w, h);
       }
     });
   }
